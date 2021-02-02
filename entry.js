8382ca9d844bf7fb79a22c1ceeb3f57f783d851a
biancadumschat <biancadumschat@gmail.com>
2021-02-02 15:50:48 +0100

    entry page

---
+++ entry.js
@@ -0,0 +1,5 @@
+const start = document.querySelector("#start");
+
+start.addEventListener("click", function () {
+    window.location.href = "file:///Users/biancadumschat/Desktop/Interactive%20Diary%20Latest/loginPage.html";
+})
